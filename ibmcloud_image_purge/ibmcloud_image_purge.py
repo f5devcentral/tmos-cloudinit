36e7703feaa63470b82281fc0950b5bf4a2e3ba0
John Gruber <j.gruber@f5.com>
2023-01-25 16:28:48 -0600

    adedd bucket naming convention for location to ibmcloud image purge

--- ibmcloud_image_purge/ibmcloud_image_purge.py
+++ ibmcloud_image_purge/ibmcloud_image_purge.py
@@ -156,7 +156,7 @@ def get_cos_buckets(location):
     cos_buckets = []
     try:
         for bucket in cos_res.buckets.all():
-            if bucket.name.startswith(COS_BUCKET_PREFIX):
+            if bucket.name.startswith(COS_BUCKET_PREFIX) and location in bucket.name:
                 cos_buckets.append(bucket)
         return cos_buckets
     except ClientError as client_error:
@@ -461,14 +461,12 @@ def sync_cloud_from_dir():
             cloud_names = get_cloud_image_names_from_patched_dir(TMOS_IMAGE_DIR, region)
             LOG.debug('cloud should have these images: %s' % cloud_names)
             for image in get_images(token, region):
-                LOG.debug(json.dumps(image))
                 if image['visibility'] == 'public' and image['owner_type'] == 'user' and image['name'] not in cloud_names:
                     images_to_delete.append(image)
             buckets_to_delete = []
             bucket_names = get_cloud_bucket_names_from_patched_dir(TMOS_IMAGE_DIR, region)
             LOG.debug('cloud should have these bucket names: %s' % bucket_names)
             for bucket in get_cos_buckets(region):
-                LOG.debug(json.dumps(bucket))
                 if bucket.name not in bucket_names:
                     buckets_to_delete.append(bucket)
             for image in images_to_delete:
